3e828fc200d64d403c9eb5607f05da17f8cdadc9
Mishka262757830 <262757830mishka@gmail.com>
2023-03-29 17:46:04 +0300

    test commit

---
+++ Cubes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cubes
+//
+//  Created by Mikhail Stepanov on 29.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
